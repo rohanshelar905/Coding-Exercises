aaebe4e8591fb8e21eb2d8c8782512ac56629b9d
Rohan Shelar <59189130+rohanshelar905@users.noreply.github.com>
2020-01-03 22:57:44 +0530

    Create ReadFile.java

---
+++ JAVA/Files/ReadFile.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFile {
+
+	public static void main(String[] args) {
+
+		File file = new File("C:\\Users\\ADMIN\\workspace\\Rohan\\src\\files\\rndfile.txt"); 
+		  
+		BufferedReader br = null;
+	    int i=0;
+		try {
+			br = new BufferedReader(new FileReader(file));
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+		  
+		String str; 
+		try {
+			while ((str = br.readLine()) != null) 
+			    System.out.println(++i+". "+str);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+}
